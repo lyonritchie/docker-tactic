0a1350673bf3016b37377136528064e5fc2f06c6
Gary Ritchie <gary@lyonritchie.com>
2020-08-05 08:01:35 -0400

    Add postgres client

--- build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-TIMESTAMP=`date +%Y-%m-%d`
-alias docker='sudo docker'
-sed -i "s/ENV REFRESHED_AT .*$/ENV REFRESHED_AT $TIMESTAMP/" Dockerfile
-docker build -t diegocortassa/tactic .
-docker build -t diegocortassa/tactic:4.7.0.b02 .
-docker push diegocortassa/tactic
